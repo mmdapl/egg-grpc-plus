84bfcb7d839a228d8a8004d76c8a00e55b1662e4
mmdapl <2237221210@qq.com>
2020-07-07 22:48:25 +0800

    添加grpc的clien配置参数

--- config/config.default.js
+++ config/config.default.js
@@ -6,5 +6,35 @@
  * @property {String} SOME_KEY - some description
  */
 exports.grpcPlus = {
+    // grpc module load config loadOptions
+    options:{
+
+    },
+    // just only one client , can provide config like this
+    client:{
+        app:true,
+        agent:false,
+        host:'127.0.0.1',
+        port:50001,
+        protoPath:'app/proto'
+    },
+    // two or more client ,can provide configs like those ,but you need choose your config by client's counts
+    clients:{
+        serviceOne:{
+            app:true,
+            agent:false,
+            host:'127.0.0.1',
+            port:50001,
+            protoPath:'app/proto'
+        },
+        serviceTwo:{
+            app:true,
+            agent:false,
+            host:'127.0.0.1',
+            port:50001,
+            protoPath:'app/proto'
+        }
+    }
+
 
 };
