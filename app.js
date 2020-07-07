8e7bac8039c285bf153d7bbf6e7b551cb60e15af
mmdapl <2237221210@qq.com>
2020-07-07 22:57:10 +0800

    添加插件加载入口

---
+++ app.js
@@ -0,0 +1,15 @@
+/*
+ * @Author: Rong姐姐好可爱
+ * @Date: 2020-07-07 22:50:39
+ * @LastEditTime: 2020-07-07 22:54:35
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \egg-grpc-plus\app.js
+ */ 
+'use strict';
+
+
+module.exports=app=>{
+    // load client and server module of grpc by different config
+    
+}
